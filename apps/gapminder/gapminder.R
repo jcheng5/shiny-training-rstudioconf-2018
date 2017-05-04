0898259b8e7204824eb742b44d947422c870c657
mine-cetinkaya-rundel <mine@stat.duke.edu>
2017-05-04 02:18:32 -0700

    add gapminder app for module demo

---
+++ apps/gapminder/gapminder.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(dplyr)
+source("data.R")
+source("gapModule.R")
+
+# UI --------------------------------------------------------------------------
+ui <- fluidPage(
+  tags$style(type="text/css", ".recalculating { opacity: 1.0; }"),
+  titlePanel("Gapminder"),
+  tabsetPanel(id = "continent", 
+    tabPanel("All", gapModuleUI("all")),
+    tabPanel("Africa", gapModuleUI("africa")),
+    tabPanel("Americas", gapModuleUI("americas")),
+    tabPanel("Asia", gapModuleUI("asia")),
+    tabPanel("Europe", gapModuleUI("europe")),
+    tabPanel("Oceania", gapModuleUI("oceania"))
+  )
+)
+
+# Server ----------------------------------------------------------------------
+server <- function(input, output) {
+  callModule(gapModule, "all", all_data)
+  callModule(gapModule, "africa", africa_data)
+  callModule(gapModule, "americas", americas_data)
+  callModule(gapModule, "asia", asia_data)
+  callModule(gapModule, "europe", europe_data)
+  callModule(gapModule, "oceania", oceania_data)    
+}
+
+# Run the application ---------------------------------------------------------
+shinyApp(ui = ui, server = server)
