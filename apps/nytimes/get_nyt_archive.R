5b39da0310cd842e5871e82665efef7c039c356b
Joe Cheng <joe@rstudio.com>
2018-01-28 15:07:09 -0800

    Fix get_nyt_archive code

--- apps/nytimes/get_nyt_archive.R
+++ apps/nytimes/get_nyt_archive.R
@@ -1,4 +1,8 @@
-get_nyt_archive <- function(year, month, day, api_key ="75f29a4d07eb423096785401b763c22d")
+library(magrittr)
+library(dplyr)
+library(lubridate)
+
+get_nyt_archive <- function(year, month, day, api_key ="d90e7a004a4e47a488a524c4801c6e27")
 {
   year = as.integer(year)
   month = as.integer(month)
@@ -16,12 +20,12 @@ get_nyt_archive <- function(year, month, day, api_key ="75f29a4d07eb423096785401
   
   date = paste(year, month, day, sep="/") %>% ymd()
   
-  d = fromJSON(url,flatten = TRUE)$response$docs
+  d = jsonlite::fromJSON(url,flatten = TRUE)$response$docs
   res = d %>%
     select(web_url,lead_paragraph, print_page, multimedia,
            pub_date, headline = headline.main) %>%
     filter(ymd_hms(pub_date) == date, print_page == 1L) %>%
-    mutate(headline = stri_trans_totitle(headline))
+    mutate(headline = stringi::stri_trans_totitle(headline))
   
   stopifnot(is.data.frame(res))
   
